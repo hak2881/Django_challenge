0ff94791444a7ad72ce79a0407db48ca8376f020
hak1319 <gkr054@naver.com>
2025-01-31 15:11:25 +0900

    ✨chapter_08 챌린지

---
+++ chapter_08/config/urls.py
@@ -0,0 +1,49 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from todo.views import todo_list, todo_update, todo_delete
+from todo.views import todo_info, todo_create
+from member import views
+from todo import cb_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    #FBV
+    # path('', todo_list, name = 'todo_list'),
+    # path('<int:pk>/', todo_info, name='todo_info'),
+    # path('create/', todo_create, name='todo_create'),
+    # path('<int:pk>/', todo_update, name='todo_update'),
+    # path('<int:pk>/delete/', todo_delete, name='todo_delete'),
+
+    path('', include('todo.urls')),
+
+    path('signup/', views.SignupView.as_view(), name= 'signup'),
+    path('accounts/', include("django.contrib.auth.urls")), # 이걸해줘야
+
+    path('login/', views.LoginView.as_view(), name = 'login'),
+    path('verify/', views.verify_email, name='verify_email'),
+
+    path('summernote/', include('django_summernote.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ chapter_08/todo/cb_views.py
@@ -0,0 +1,148 @@
+from django.http.response import HttpResponseRedirect, Http404
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
+from todo.models import Todo, Comment
+from django.db.models.query_utils import Q
+from django.contrib.auth.mixins import LoginRequiredMixin
+from todo.form import CommentForm, TodoForm
+
+
+
+class TodoListView(LoginRequiredMixin,ListView):
+    model = Todo
+    template_name = 'todo_list.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(author=self.request.user)
+
+        q = self.request.GET.get('q')
+
+        if q:
+            queryset = queryset.filter(
+                Q(title__icontains=q)|
+                Q(description__icontains=q)
+            )
+        return queryset
+
+class TodoDetailView(LoginRequiredMixin,DetailView):
+    model = Todo
+    template_name = 'todo_info.html'
+    paginate_by = 10
+
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        todo = self.object # pk
+        context['comments'] = self.object.comments.prefetch_related('author').all()
+
+        context['data'] = {
+            'title': todo.title,
+            'description': todo.description,
+            'created_at': todo.created_at,
+            'start_date': todo.start_date,
+            'end_date': todo.end_date,
+            'modified_at': todo.modified_at,
+            'is_completed': todo.is_completed,
+            'image' : todo.image.url if todo.image else None,
+        }
+        return context
+
+    def post(self, *args, **kwargs):
+        comment_form = CommentForm(self.request.POST)
+        self.object = self.get_object()  # 디테일에서 제공하는 메서드로 객체 가져오기
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.todo = self.object
+            comment.author = self.request.user
+            comment.todo_id = self.kwargs['pk']
+            comment.save()
+
+            return HttpResponseRedirect(reverse_lazy('todo:info', kwargs={'pk': self.object.pk}))
+
+        context = self.get_context_data()
+        context['comment_form'] = comment_form
+        return self.render_to_response(context)
+
+
+
+class TodoCreateView(LoginRequiredMixin, CreateView):
+    model = Todo
+    template_name = 'todo_create.html'
+    form_class = TodoForm
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse_lazy('todo:info', kwargs={'pk': self.object.pk})
+
+class TodoUpdateView(LoginRequiredMixin, UpdateView):
+
+    model = Todo
+    template_name = 'todo_update.html'
+    form_class = TodoForm
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if self.request.user.is_staff:
+            return queryset
+        return queryset.filter(author=self.request.user)
+
+    def get_success_url(self):
+        return reverse_lazy('todo:info', kwargs={'pk':self.object.pk})
+
+class TodoDeleteView(LoginRequiredMixin, DeleteView):
+    model = Todo
+    def get_queryset(self):
+        queryset= super().get_queryset()
+
+        if not self.request.user.is_staff:
+            return queryset.filter(author=self.request.user)
+        return queryset
+
+
+    def get_success_url(self):
+        return reverse_lazy('todo:list')
+
+
+class CommentUpdateView(LoginRequiredMixin, UpdateView):
+    model = Comment
+    template_name = 'comment_form.html'
+    fields = ['content']
+
+    def get_object(self, queryset = None):
+        obj = super().get_object(queryset)
+
+        if obj.author != self.request.user :
+            raise Http404
+        return obj
+
+    def get_success_url(self):
+        return reverse_lazy('todo:info', kwargs={'pk': self.object.todo.pk})
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context['sub_title'] ='수정'
+        context['btn_name'] ='수정'
+        return context
+
+class CommentDeleteView(LoginRequiredMixin, DeleteView):
+    model = Comment
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+
+        if obj.author != self.request.user and not self.request.user.is_superuser:
+            raise Http404
+        return obj
+
+    def get_success_url(self):
+        return reverse_lazy("todo:info", kwargs={"pk": self.object.todo.pk})
